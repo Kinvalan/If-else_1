b204c9a12127456c11edfa72f5a60f70dfbc0dc1
Kinvalan <sebastianmoller01@icloud.com>
2023-05-05 15:30:18 +0200

    Added comments for myself

--- Program.cs
+++ Program.cs
@@ -11,34 +11,41 @@ namespace If_else
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Skriv inn det første tallet: ");
-            int tall1 = int.Parse(Console.ReadLine()); 
+            /*
+             Konsollen viser brukeren en tekst
+             Vi sier at int-en er lik int.Parse() og sender inn Console.ReadLine
+             Det gjør at tall1 og tall2 er lik det vi skriver inn i konsollen
+             Det vi skrev inn blir nå tolka som en int verdi (heltall) og ikke string, pga int.Parse()
+            */
 
+            Console.WriteLine("Skriv inn det første tallet: "); 
+            int tall1 = int.Parse(Console.ReadLine());          
+            
             Console.WriteLine("Skriv inn det andre tallet: ");
             int tall2 = int.Parse(Console.ReadLine());
 
-            bool AreAlike = NumbersAreAlike(tall1, tall2);
+            bool AreAlike = NumbersAreAlike(tall1, tall2); // Lager en bool som er lik funksjonen vår for å kalle den enklere.
 
 
-            if (AreAlike)
+            if (AreAlike) // Hvis det brukeren skriver inn som tall1 er lik det som blir lik tall2
             {
-                Console.WriteLine("Tallene er like.");
+                Console.WriteLine("Tallene er like."); // Så vil konsollen skrive at tallene er like.
             }
-            else
+            else 
             {
-                Console.WriteLine("Tallene er ikke like.");
+                Console.WriteLine("Tallene er ikke like."); // Om tallene ikke er like vil konsollen skrive det.
             }
         }
 
-        static bool NumbersAreAlike(int tall1, int tall2) 
+        static bool NumbersAreAlike(int tall1, int tall2) // Sjekker om tallene man skriver inn er like. Tar imot to parametre.
         {
-            if(tall1 == tall2)
+            if(tall1 == tall2) 
             {
-                return true;
+                return true; // Er tall1 likt tall2 returneres true og konsollen skriver at tallene er like.
             }
             else
             {
-                return false;
+                return false; // Hvis tallene ikke er like så vil konsollen si det og returnere false.
             }
         } 
     }
